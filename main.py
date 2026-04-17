a6d3cba6b84dead0f4067c3c63fee188ebf9ed46
erlendromo <erlendromo@hotmail.com>
2026-04-17 16:34:50 +0200

    feat: Added current implementation pipeline to main (WIP)

--- main.py
+++ main.py
@@ -1,15 +1,78 @@
+from os import getenv
+from time import time
+
+from cv2 import imread
+from dotenv import load_dotenv
+
 from src.classifier import Classifier
+from src.dataset import Dataset
 from src.feature_extractor import FeatureExtractor
+from src.feature_scaler import FeatureScaler
 from src.preprocessor import Preprocessor
 
-"""
-Trashnet project entry point
-"""
+# Load .env file
+load_dotenv()
+
+
+def debug_mode():
+    debug = getenv("DEBUG", False)
+    debug = True if debug == "True" else False
+    if debug:
+        print("Debug mode activated...\n")
+
+    return debug
+
+
+def elapsed(start):
+    end = time()
+    elapsed = end - start
+
+    elapsed_minutes = elapsed / 60 if elapsed >= 60 else 0
+    elapsed_seconds = elapsed % 60 if elapsed >= 60 else elapsed if elapsed < 60 else 0
+
+    print(f"Elapsed time: {elapsed_minutes}min and {elapsed_seconds:.0f}sec")
+
+
+def extract_features(dataset, debug):
+    features = []
+    labels = []
+    for path, label in dataset:
+        image = imread(path)
+        if image is None:
+            continue
+
+        processed_image = Preprocessor(image=image, debug=debug).process()
+        image_features = FeatureExtractor(processed_image, debug=debug).extract()
+
+        features.append(image_features)
+        labels.append(label)
+
+    return features, labels
 
 
 def main():
-    preprocessor = Preprocessor()
-    preprocessor.print()
+    start = time()
+    debug = debug_mode()
+
+    dataset = Dataset(debug=debug)
+    training_set, validation_set, testing_set = dataset.load_and_split()
+
+    training_features, training_labels = extract_features(
+        dataset=training_set, debug=debug
+    )
+    validation_features, validation_labels = extract_features(
+        dataset=validation_set, debug=debug
+    )
+    testing_features, testing_labels = extract_features(
+        dataset=testing_set, debug=debug
+    )
+
+    feature_scaler = FeatureScaler()
+    scaled_training_features = feature_scaler.fit_transform(training_features)
+    scaled_validation_features = feature_scaler.transform(validation_features)
+    scaled_testing_features = feature_scaler.transform(testing_features)
+
+    elapsed(start)
 
 
 if __name__ == "__main__":
