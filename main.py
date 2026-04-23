a4b2f79fbc3464b15fbe85d860138dd7858add83
erlendromo <erlendromo@hotmail.com>
2026-04-23 18:12:49 +0200

    feat: Updated main entrypoint

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 from dotenv import load_dotenv
-
 from src.pipeline import Pipeline
 from src.utils.config import Config
 
 
 def main():
     load_dotenv("./.env")
-
     config = Config()
-    config.print()
 
-    pipeline = Pipeline(
-        dataset_path=config.dataset_path,
-        save_model=config.save_model,
-        load_model=config.load_model,
-        classifier_type=config.classifier_type,
-        debug=config.debug,
-    )
+    pipeline = Pipeline(config)
     pipeline.execute()
 
     config.elapsed_time()
